52f1609352d44fd36d8681b362a7f781915d3f27
karthikn25 <karthikn25.dev@gmail.com>
2025-04-14 12:23:08 +0530

    navbar added

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
+import Navbar from "./components/Navbar";
 
 const App = () => {
   return (
     <div>
       <h1>My Personal Portfolio under Development</h1>
+      <Navbar />
     </div>
   );
 };
