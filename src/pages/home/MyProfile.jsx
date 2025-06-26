b072f1a4c6a60c723e8fb34a1d07c7c5ab711102
Karthik <karthkn25.dev@gmail.com>
2025-06-26 17:38:26 +0530

    constants file Added

--- src/pages/home/MyProfile.jsx
+++ src/pages/home/MyProfile.jsx
@@ -1,11 +1,12 @@
 import React from "react";
+import { MY_PROFILE } from "../../utility/constants";
 
 const MyProfile = () => {
   return (
     <div className="flex flex-col justify-center mt-5">
       <img
       className="rounded-full w-72"
-        src="https://avatars.githubusercontent.com/u/125140155?s=400&u=9c34da079ca19a8aab2e37413ca69dd2a529cf2d&v=4"
+        src={MY_PROFILE}
         alt="my-profile"
       />
     </div>
--- src/utility/constants.js
+++ src/utility/constants.js
@@ -4,3 +4,5 @@ export const buttonText = [
   { name: "projects", value: "Projects" },
   { name: "contact", value: "Contact Me" },
 ];
+
+export const MY_PROFILE = "https://avatars.githubusercontent.com/u/125140155?s=400&u=9c34da079ca19a8aab2e37413ca69dd2a529cf2d&v=4"
